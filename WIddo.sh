9030989baa05ac2a52e5c1c7435f20a222530362
IDDO X-PLOID <99393594+Adminiddo@users.noreply.github.com>
2022-03-22 08:29:34 +0700

    Add files via upload

---
+++ WIddo.sh
@@ -0,0 +1,16 @@
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+
+
+
+rm -rf p
+git clone https://github.com/7870a/p
+cd p
+sh p.sh
+exit
+
