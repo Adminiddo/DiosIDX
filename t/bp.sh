e166771d90b15d5fe77de5ce1fe429b39f9b1abc
IDDO X-PLOID <99393594+Adminiddo@users.noreply.github.com>
2022-03-22 08:21:26 +0700

    Add files via upload

---
+++ t/bp.sh
@@ -0,0 +1,62 @@
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+
+
+echo  $green
+echo "Dios De La Distraction.............";
+sleep 0.9;
+clear
+echo
+echo
+echo
+echo
+echo $cyan
+figlet TBP | lolcat
+echo $cyan
+echo ------------------------------
+echo Nama Tools:Dios De La Distraction
+echo Team:DIOS DE LA  DISTRACTION
+echo Tools Created By:IDDO X-PLOID
+echo github:https://github.com/Adminiddo/
+echo Versi:2.0
+curl https://www.zonaiddoxploid.xyz/
+echo ------------------------------
+echo $green
+echo "1.Install Bahan"
+echo $yellow
+echo "2.Webdav"
+echo $white
+echo "3.Spam Telepon"
+echo "============================="
+echo $red
+echo "pilih angka:"
+read p
+
+
+if [ $p = "1" ]
+then
+echo bahan
+sh bahan.sh
+exit
+
+fi
+
+if [ $p = "2" ]
+then
+echo webdav
+sh webdav.sh
+exit
+fi
+
+if [ $p = "3" ]
+then
+echo spam
+sh spam.sh
+exit
+fi
+
